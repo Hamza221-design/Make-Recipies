8802e20aa061e7fb58d55236f7dfd2bc03664ce6
Hamza Choudhary <hamuu844@gmail.com>
2024-12-21 23:58:54 +0500

    Update script.js

--- script.js
+++ script.js
@@ -75,7 +75,7 @@ recipeCloseBtn.addEventListener('click', ()=>{
 searchBtn.addEventListener('click', (e)=> {
     e.preventDefault();
     const searchInput = searchBox.value.trim();
-    if(searchInput){
+    if(!searchInput){
        recipeContainer.innerHTML= `<h2> Type the meal in the search box.</h2>`;
        return;
     }
